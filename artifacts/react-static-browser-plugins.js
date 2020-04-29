1b3443d25334f7162fcb2c801b0a0e3607b8b96f
courington <chase.courington@gmail.com>
2020-04-29 16:15:56 -0600

    remove jekyll replace with react-static

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,27 @@
+// Imports
+import plugin0 from '/Users/chasecourington/Development/courington.github.io/node_modules/react-static-plugin-reach-router/browser.api.js'
+
+// Plugins
+const plugins = [{
+        location: "/Users/chasecourington/Development/courington.github.io/node_modules/react-static-plugin-source-filesystem",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/chasecourington/Development/courington.github.io/node_modules/react-static-plugin-reach-router",
+        plugins: [],
+        hooks: plugin0({})
+      },
+{
+        location: "/Users/chasecourington/Development/courington.github.io/node_modules/react-static-plugin-sitemap/dist",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/chasecourington/Development/courington.github.io",
+        plugins: [],
+        hooks: {}
+      }]
+
+// Export em!
+export default plugins
