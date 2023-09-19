a6068794f7ce642eca91d7276bb1e7853d2e7ed5
courington <chase.courington@gmail.com>
2023-09-19 15:55:29 -0400

    migrate to svelte for gh-pages

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Chase Courington - Software Engineer</title>
+	<meta name="description" content="Chase Courington - CV" />
+</svelte:head>
+
+<section>
+	<h1>Chase Courington</h1>
+	<h2>Software Engineer</h2>
+</section>
+
+<style>
+	h1 {
+		font-family: var(--font-body);
+	}
+</style>
